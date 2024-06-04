371d5a344d66e5004b53c0579156e4b96d24681e
KatrinKuzyutkina <Kuzyutckina.ekaterina@yandex.ru>
2024-06-04 16:19:30 +0300

    5 commit

--- app/root.c
+++ app/root.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "roots.h"
+#include "root.h"
 
 int roots(double* arr, double a, double b, double c){
     double d = b * b - 4 * a * c;
